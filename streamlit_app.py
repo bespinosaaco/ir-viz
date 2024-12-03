790e69a521aaa5a0050d6b93faf727224855193c
Brian Espinosa-Acosta <brianespinosaacosta@gmail.com>
2024-12-03 14:20:05 -0330

    Mockup version

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 import pandas as pd
 import io
 import plotly.graph_objects as go
+import numpy as np
 
 # Set page configuration
 st.set_page_config(layout="wide")
@@ -86,32 +87,44 @@ def Norm(ATR, min_value=0, max_value=1):
     ATR['norm'] = ((ATR.iloc[:, 1] - min_val) / (max_val - min_val)) * (max_value - min_value) + min_value
     return ATR
 
-# The app begins here
+# The app begins here!!!
 st.title("Welcome to AC/BC 🦦 Viz")
+st.caption("Scroll down to see all the graphics")
+st.caption("The graphs are interactives and downloadables")
 
 with st.spinner('Connecting to Brian NextCloud...'):
     master = get_csv_file_as_dataframe("/master.csv")
     file_list = list_nextcloud_folder_files("/processed")
 
 with st.sidebar:
-    choice = st.multiselect('Select sample', options=master['Code'], placeholder='Code')
+    choice = st.multiselect('Select sample(s)', options=master['Code'], placeholder='Code',
+                            help='Select the samples you wish to compare')
     param = st.selectbox('Parameter', options=['BET','pH','Yield','pore size','pore volume'],
+                         help= 'Select one of the parameters',
                            placeholder='Parameter')
 st.write('Connected to Brian NextCloud')
 st.write('''
 ### Master Biochar Inventory 📖
 ''')
-
+st.caption('This is the Biochar Inventory. You can sort, search, expand and download')
 st.dataframe(master,use_container_width=True)
 
 st.write('''---
 ### Visualize information from the selected samples 👇 
 ---''')
-col1, col2, col3 = st.columns((1,1,2))
+col1, col2, col3 = st.columns((1,2,3))
 if choice and param:
     with col1:
         st.dataframe(master[['Code',param]][master['Code'].isin(choice)],
-                     use_container_width=False)
+                     use_container_width=True)
+    with col2:
+        st.write(f'''
+        ### **{param} Method**  
+        Some description of the methodology used to record this data.  
+        Step-by-step:  
+        1. This is step 1  
+        2. This is step 2
+''')
     with col3:
         fig = go.Figure(data=[
             go.Bar(name='Sample Data', x=choice, y=master[param][master['Code'].isin(choice)])
@@ -126,48 +139,77 @@ if choice and param:
         st.plotly_chart(fig,use_container_width=True)
 else:
     st.warning('Select the samples and parameters on the sidebar 👈')
-# # Load data based on selection
-# sample_dic = {}
-# option_c = None
-# if choice:
-#     if 'All' in choice:
-#         choice = file_list
-#     sample_dic = load_and_normalize_data(choice)
-#     option_c = choice
-# else:
-#     st.write("None selected")
-#
-# # Display DataFrame based on user selection
-# if option_c:
-#     option_df = st.selectbox('Select a DataFrame to show', option_c, placeholder='Select .dpt')
-# else:
-#     option_df = None
-#
-# # Display DataFrame button
-# if st.button('DataFrame'):
-#     if option_df is not None:
-#         st.write(sample_dic[option_df])
-#     else:
-#         st.warning("Select an option first")
-#
-# # Plot button
-# if st.button('Plot'):
-#     if option_c:
-#         fig = go.Figure()
-#         y = 'norm' if plot_norm else 'Intensity'
-#         for i in sample_dic.keys():
-#             fig.add_trace(go.Scatter(x=sample_dic[i]['Wavenumber'], y=sample_dic[i][y],
-#                                      mode='lines', name=i[:-4]))
-#
-#         fig.update_layout(
-#             xaxis=dict(range=[4000, 400]),  # Inverted x-axis range as in the original plot
-#             title='ATR-FTIR',
-#             xaxis_title='Wavelength',
-#             yaxis_title='Intensity (a.u)' if not plot_norm else 'Normalized Intensity',
-#             width=1200,
-#             height=800
-#         )
-#
-#         st.plotly_chart(fig, use_container_width=True)
-#     else:
-#         st.warning("Select a graph to plot! 👈")
+
+
+st.write(r'''---
+### Adsorption, SSA, $(O+N)/C$  
+''')
+st.warning('Simulated data for displaying')
+# Generate random data for the 3D scatter plot
+num_points = 100
+x = np.random.uniform(10, 300, num_points)
+y = np.random.uniform(10, 300, num_points)
+z = np.random.uniform(10, 300, num_points)
+
+# Create a 3D scatter plot with Plotly
+# Create labels for each data point
+labels = [f"acbc{i}: ({z[i]:.2f}, {x[i]:.2f}, {y[i]:.2f})" for i in range(num_points)]
+fig = go.Figure(data=[go.Scatter3d(
+    x=x,
+    y=y,
+    z=z,
+    mode='markers',
+    marker=dict(
+        size=5,
+        color=z,  # Color by z values
+        colorscale='Viridis',
+        opacity=0.8),
+    text=labels,  # Add labels for hover
+    hoverinfo='text'  # Display only the text on hover
+
+)])
+
+# Customize layout
+fig.update_layout(
+    paper_bgcolor="rgba(230, 230, 230, 0.8)",
+    width=800,
+    height=800,
+    scene=dict(
+    xaxis_title='(N+O)/C',
+    yaxis_title='SSA',
+    zaxis_title='Adsorption',
+))
+
+# 3D scatter plot
+st.plotly_chart(fig,use_container_width=True)
+
+st.write(r'''---
+### Infrared spectroscopy  
+''')
+st.warning("Brian's data. Not the one in the master spreadsheet yet! ⚒️")
+
+# Load data based on selection
+sample_dic = {}
+option_c = None
+choice = file_list
+sample_dic = load_and_normalize_data(choice)#
+y = 'Intensity'
+on = st.toggle("Plot Normalized")
+
+if on:
+    y = 'norm'
+fig = go.Figure()
+for i in sample_dic.keys():
+    fig.add_trace(go.Scatter(x=sample_dic[i]['Wavenumber'], y=sample_dic[i][y],
+                             mode='lines', name=i[:-4]))
+
+fig.update_layout(
+    xaxis=dict(range=[4000, 400]),
+    title='ATR-FTIR',
+    xaxis_title='Wavelength',
+    yaxis_title='Intensity (a.u)',
+    width=1200,
+    height=800
+)
+
+st.plotly_chart(fig, use_container_width=True)
